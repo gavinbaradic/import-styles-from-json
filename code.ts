721aa3426b0970381afc5ee0ad66871c53ec2893
BARADIC, GAVIN <gavinbaradic@northwesternmutual.com>
2021-04-16 07:25:37 -0500

    feat: react boilerplate

--- code.ts
+++ code.ts
@@ -5,17 +5,17 @@ const numberOfRectangles = 5
 // You can access browser APIs such as the network by creating a UI which contains
 // a full browser environment (see documentation).
 
-const nodes: SceneNode[] = [];
+const nodes: SceneNode[] = []
 for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+  const rect = figma.createRectangle()
+  rect.x = i * 150
+  rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }]
+  figma.currentPage.appendChild(rect)
+  nodes.push(rect)
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
+figma.currentPage.selection = nodes
+figma.viewport.scrollAndZoomIntoView(nodes)
 
 // Make sure to close the plugin when you're done. Otherwise the plugin will
 // keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
+figma.closePlugin()
