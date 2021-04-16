70094c0dc5ad1bfcfc5dc48b50073ca7bc533ec6
BARADIC, GAVIN <gavinbaradic@northwesternmutual.com>
2021-04-16 07:27:05 -0500

    chore: formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import * as React from 'react';
-import { Page, Rectangle, Text } from 'react-figma';
+import * as React from 'react'
+import { Page, Rectangle, Text } from 'react-figma'
 
 export const App = () => {
-    return (
-        <Page isCurrent name="Page X">
-            <Text characters="Hello, react-figma!"/>
-            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#0ddd25' }} />
-            <Rectangle style={{ width: 200, height: 100, backgroundColor: '#a6dd00' }} />
-        </Page>
-    );
-};
+  return (
+    <Page isCurrent name='Page X'>
+      <Text characters='Hello, react-figma!' />
+      <Rectangle
+        style={{ width: 200, height: 100, backgroundColor: '#0ddd25' }}
+      />
+      <Rectangle
+        style={{ width: 200, height: 100, backgroundColor: '#a6dd00' }}
+      />
+    </Page>
+  )
+}
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { App } from './App';
+import * as React from 'react'
+import { App } from './App'
 
-import 'react-figma/rpc';
-import { render } from 'react-figma';
+import 'react-figma/rpc'
+import { render } from 'react-figma'
 
-render(<App />);
+render(<App />)
